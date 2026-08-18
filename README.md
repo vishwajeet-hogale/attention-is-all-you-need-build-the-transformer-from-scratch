c2cee985040141854f451f2f0a97825e575fa940
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2026-08-18 00:03:18 -0700

    Step 38: apply_dropout_with_keep_mask

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ python scaffold.py
 - [x] **35.** compute_layer_norm_mean_and_variance
 - [x] **36.** normalize_and_scale_with_gamma_beta
 - [x] **37.** apply_residual_add_and_norm
-- [ ] **38.** apply_dropout_with_keep_mask
+- [x] **38.** apply_dropout_with_keep_mask
 - [ ] **39.** encoder_layer_self_attention_sublayer
 - [ ] **40.** encoder_layer_feed_forward_sublayer
 - [ ] **41.** assemble_encoder_layer
--- model.py
+++ model.py
@@ -400,8 +400,10 @@ def apply_residual_add_and_norm(residual_input, sublayer_output, gamma, beta, ep
     output = residual_input + sublayer_output
     return normalize_and_scale_with_gamma_beta(output, gamma, beta, eps)
 
-# Step 38 - apply_dropout_with_keep_mask (not yet solved)
-# TODO: implement
+# Step 38 - apply_dropout_with_keep_mask
+def apply_dropout_with_keep_mask(x, keep_mask, keep_prob):
+    # TODO: multiply x by the boolean keep_mask and rescale by 1/keep_prob.
+    return keep_mask * x / keep_prob
 
 # Step 39 - encoder_layer_self_attention_sublayer (not yet solved)
 # TODO: implement
