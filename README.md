380907f987d6b9eb6a104f77c6e5329bc8775f06
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2026-08-17 23:58:50 -0700

    Step 36: normalize_and_scale_with_gamma_beta

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ python scaffold.py
 - [x] **33.** apply_ffn_second_linear
 - [x] **34.** position_wise_feed_forward_network
 - [x] **35.** compute_layer_norm_mean_and_variance
-- [ ] **36.** normalize_and_scale_with_gamma_beta
+- [x] **36.** normalize_and_scale_with_gamma_beta
 - [ ] **37.** apply_residual_add_and_norm
 - [ ] **38.** apply_dropout_with_keep_mask
 - [ ] **39.** encoder_layer_self_attention_sublayer
--- model.py
+++ model.py
@@ -382,8 +382,15 @@ def compute_layer_norm_mean_and_variance(x):
     var = x.var(dim=-1, keepdim=True, correction=0)
     return mean, var
 
-# Step 36 - normalize_and_scale_with_gamma_beta (not yet solved)
-# TODO: implement
+# Step 36 - normalize_and_scale_with_gamma_beta
+import torch
+import math
+
+def normalize_and_scale_with_gamma_beta(x, gamma, beta, eps=1e-5):
+    # TODO: standardize x along the last axis then apply gamma and beta affine transform
+    mean, var = compute_layer_norm_mean_and_variance(x)
+    x_cap = (x - mean) / torch.sqrt(var + eps)
+    return gamma * x_cap + beta
 
 # Step 37 - apply_residual_add_and_norm (not yet solved)
 # TODO: implement
